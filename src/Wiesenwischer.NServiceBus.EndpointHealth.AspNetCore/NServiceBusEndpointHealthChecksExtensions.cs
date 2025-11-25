2830b88dd22c2bb5e41af1dad7e6837684642a23
Marcus Dammann <m.dammann@ams-erp.com>
2025-11-25 16:56:55 +0100

    Improve defaults and ASP.NET Core options registration
    - Change UnhealthyAfter default from 2min to 3min (allows 2-3 missed pings)
    - AddNServiceBusEndpointHealth now registers default EndpointHealthOptions
    - New overload with configureOptions for custom PingInterval/UnhealthyAfter

--- src/Wiesenwischer.NServiceBus.EndpointHealth.AspNetCore/NServiceBusEndpointHealthChecksExtensions.cs
+++ src/Wiesenwischer.NServiceBus.EndpointHealth.AspNetCore/NServiceBusEndpointHealthChecksExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Wiesenwischer.NServiceBus.EndpointHealth.AspNetCore;
@@ -17,9 +18,18 @@ public static class NServiceBusEndpointHealthChecksExtensions
     /// <param name="tags">Optional tags to associate with the health check.</param>
     /// <returns>The health checks builder for chaining.</returns>
     /// <remarks>
-    /// This health check requires that <see cref="IEndpointHealthState"/> and <see cref="EndpointHealthOptions"/>
-    /// are registered in the service collection. These are automatically registered when using
-    /// <c>EnableEndpointHealth()</c> on your NServiceBus endpoint configuration.
+    /// <para>
+    /// This health check requires that <see cref="IEndpointHealthState"/> is registered in the service collection.
+    /// </para>
+    /// <para>
+    /// For NServiceBus 8.x (.NET 9+), both <see cref="IEndpointHealthState"/> and <see cref="EndpointHealthOptions"/>
+    /// are automatically registered when using <c>EnableEndpointHealth()</c>.
+    /// </para>
+    /// <para>
+    /// For NServiceBus 7.x (.NET Core 3.1), you must manually register <see cref="IEndpointHealthState"/>
+    /// and pass it to <c>EnableEndpointHealth(healthState, ...)</c>. The <see cref="EndpointHealthOptions"/>
+    /// will be registered with default values if not already present.
+    /// </para>
     /// </remarks>
     public static IHealthChecksBuilder AddNServiceBusEndpointHealth(
         this IHealthChecksBuilder builder,
@@ -27,6 +37,36 @@ public static class NServiceBusEndpointHealthChecksExtensions
         HealthStatus? failureStatus = null,
         IEnumerable<string>? tags = null)
     {
+        // Register default options if not already registered
+        // This is needed for NServiceBus 7.x where DI containers are separate
+        builder.Services.TryAddSingleton<EndpointHealthOptions>();
+
+        return builder.AddCheck<NServiceBusEndpointHealthCheck>(
+            name,
+            failureStatus ?? HealthStatus.Unhealthy,
+            tags ?? ["nservicebus", "endpoint", "messaging"]);
+    }
+
+    /// <summary>
+    /// Adds the NServiceBus endpoint health check to the health check builder with custom options.
+    /// </summary>
+    /// <param name="builder">The health checks builder.</param>
+    /// <param name="configureOptions">Action to configure the health options.</param>
+    /// <param name="name">The name of the health check. Defaults to "nservicebus-endpoint".</param>
+    /// <param name="failureStatus">The failure status to report. Defaults to <see cref="HealthStatus.Unhealthy"/>.</param>
+    /// <param name="tags">Optional tags to associate with the health check.</param>
+    /// <returns>The health checks builder for chaining.</returns>
+    public static IHealthChecksBuilder AddNServiceBusEndpointHealth(
+        this IHealthChecksBuilder builder,
+        Action<EndpointHealthOptions> configureOptions,
+        string name = "nservicebus-endpoint",
+        HealthStatus? failureStatus = null,
+        IEnumerable<string>? tags = null)
+    {
+        var options = new EndpointHealthOptions();
+        configureOptions(options);
+        builder.Services.AddSingleton(options);
+
         return builder.AddCheck<NServiceBusEndpointHealthCheck>(
             name,
             failureStatus ?? HealthStatus.Unhealthy,
--- src/Wiesenwischer.NServiceBus.EndpointHealth/EndpointHealthOptions.cs
+++ src/Wiesenwischer.NServiceBus.EndpointHealth/EndpointHealthOptions.cs
@@ -13,11 +13,12 @@ public class EndpointHealthOptions
 
     /// <summary>
     /// Gets or sets the time after which the endpoint is considered unhealthy
-    /// if no health ping has been processed. Default is 2 minutes.
+    /// if no health ping has been processed. Default is 3 minutes.
     /// </summary>
     /// <remarks>
     /// This value should be greater than <see cref="PingInterval"/> to allow
-    /// for some tolerance in message processing delays.
+    /// for some tolerance in message processing delays. The default of 3 minutes
+    /// allows for 2-3 missed pings before the endpoint is considered unhealthy.
     /// </remarks>
-    public TimeSpan UnhealthyAfter { get; set; } = TimeSpan.FromMinutes(2);
+    public TimeSpan UnhealthyAfter { get; set; } = TimeSpan.FromMinutes(3);
 }
